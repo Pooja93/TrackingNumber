2f61c26be4c5f23ccf044fef2238bb731e061601
Asha Jyothi <ashajyothi.pryr@gmail.com>
2015-07-28 09:48:45 +0530

    Split function

--- TrackingNumber/src/TrackingNumber/TrackingTableRow.java
+++ TrackingNumber/src/TrackingNumber/TrackingTableRow.java
@@ -30,4 +30,36 @@ public class TrackingTableRow {
 		
 		return null;
 	}
+	
+public splitInterval(rowObject robj1, rowObject robj2){
+    int LOW;
+    int HIGH;
+
+    if(rObj1.low<rObj2.low){
+            LOW=robj1.low;
+            HIGH=robj1.high;
+        }
+    else if(rObj1.low>rObj2.low){
+            LOW=robj2.low;
+            HIGH=robj2.high;
+        }    
+             
+    updateTableRow(LOW,(rObj2.low)-1);
+    updateTableRow(rObj2.low,rObj2.high);
+    updateTableRow(rObj2.high+1,HIGH);
+        }
+        
+    if((rObj1.low<rObj2.low)&&(rObj2.low<rObj1.high)){   //more overlap
+            updateTableRow(rObj1.low,(rObj2.low)-1);
+            updateTableRow(rObj2.low,rObj1.high);
+            updateTableRow((rObj1.high)+1,rObj2.high);
+        }
+    else if((rObj2.low<rObj1.low)&&(rObj2.high<rObj1.high)){   //less overlap
+            updateTableRow(rObj2.low,(rObj1.high)-1);
+            updateTableRow(rObj1,high,rObj2.high);
+            updateTableRow(rObj2.high+1,rObj1.high);
+        }
+        
+    }
+    
 }
